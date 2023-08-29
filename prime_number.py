8eecb2a6b9ea4286bf8b5290eadd2f8e66f32491
rama wandya <ramawandia153@gmail.com>
2023-08-29 23:30:27 +0700

    add git ignore and solution for prime number in python

---
+++ prime_number.py
@@ -0,0 +1,25 @@
+import time
+lower = int(input("masukkan angka lower :"))
+upper = int(input("masukkan angka upper :"))
+is_prime = 0
+total_prime_number =0
+start = time.time()
+for current_number in range(lower, upper+1):
+    # print('this is x ', x)
+    for divider in range(2,int(current_number/2)+1):
+        # print('this is  y ', y)
+        if current_number % divider == 0 :
+            is_prime+= 1
+            break
+        # print('ini is prime', is_prime)
+    if is_prime == 0:
+        print(str(current_number) + 'is rill prime')
+        total_prime_number += 1
+    else : 
+        print(str(current_number) + "you're not prime")
+    is_prime = 0
+
+print('total prime number in interval are : ', total_prime_number)
+end = time.time()
+
+print("execution time : ", end-start)
