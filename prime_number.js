fa795db24c7ad177fca0c1d11fea1f3e8310e492
rama wandya <ramawandia153@gmail.com>
2023-09-03 15:42:18 +0700

    add prime number finder in js

--- prime_number.js
+++ prime_number.js
@@ -2,7 +2,7 @@ primes = []
 
 let isNot_Prime = 0
 
-for (let currentNumber = 2; currentNumber <= 1000000; currentNumber++){
+for (let currentNumber = 2; currentNumber <= 10000; currentNumber++){
     for (let divider = 2; divider < currentNumber;divider++){
         // console.log(`this is current number : ${currentNumber} and divider :  ${divider}`)
         if (currentNumber % divider == 0){
