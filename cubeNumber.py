c7f62acf6b7164b61c166fc9fb7a0441180d4cc4
rama wandya <ramawandia153@gmail.com>
2023-10-08 20:51:47 +0700

    docs: add documentation for the code

--- cubeNumber.py
+++ cubeNumber.py
@@ -1,4 +1,4 @@
-val = int(input("Enter specified range number : "))
+val = int(input("Enter specified range number : "))  #get range of number by input user
 
-for x in range(1,val+1):
-    print(f"current number : {x} and the cube is : {x**3}")
+for x in range(1,val+1):  #loop from 1 to number inputted
+    print(f"current number : {x} and the cube is : {x**3}")  #print the loop current number and the cube
