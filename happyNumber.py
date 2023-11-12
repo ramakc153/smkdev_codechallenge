7992b5e6bf4467e5ee6c7507a4c8b1727e1d013b
rama wandya <ramawandia153@gmail.com>
2023-11-12 20:19:30 +0700

    fix: add documentation for happynumber

--- happyNumber.py
+++ happyNumber.py
@@ -1,15 +1,16 @@
 import time
-number = input("input the number : ")
-start = time.time()
-while int(number) != 1:
-    temp_sum = 0
+number = input("input the number : ")  #Input number to check
+start = time.time()  #variable for timeout 
+while int(number) != 1:  #create while loop while the result number not equal to 0
+    temp_sum = 0  #temp variable to assign to current number
+    # This logic will triger if code run more than 5 seconds
     if time.time() - start >=5:
         print("code exit after 5 seconds ")
         break
     for num in range(len(number)):
         # print("ini num", num)
-        temp_sum += int(number[num]) ** 2
-    number = str(temp_sum)
+        temp_sum += int(number[num]) ** 2  #sum of square number of each digit
+    number = str(temp_sum)  #assigning the number variable
     print("ini number ", number)
 
 print(number)
